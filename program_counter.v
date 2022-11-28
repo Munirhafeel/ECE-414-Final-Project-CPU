0fa2642575c90ea4f709eba689877549380d692c
Andres <andres.sebpal@gmail.com>
2022-11-28 07:56:49 -0500

    changes to program counter

--- program_counter.v
+++ program_counter.v
@@ -1,5 +1,39 @@
 module program_counter(
-
+    input DR, SB, // Source-B
+    input D, // D-Bus (MUX-D) 0 = Load Immediate || 1 = Memory
+    input [1:0] PS, // PS Signal from Control Word (Control Logic)
+    input clk, reset,
+    output [15:0] address;
 );
 
-//boilerplate
+reg [15:0] PC;
+
+// sign extend SB, DR
+
+always @ (posedge clk) begin
+    if (reset) begin
+        PC <= 16'b0'
+    end else begin
+        case (PS)
+        2'b00:
+        begin
+            PC <= PC; // hold PC value
+        end
+        2'b01:
+        begin
+            PC <= PC + 1'b1; //
+        end
+        2'b10:
+        begin
+            PC <= PC + ; // 
+        end
+        2'b11:
+        begin
+            PC <= PC; // 
+        end
+        endcase
+    end
+    address <= PC;
+end   
+
+endmodule
