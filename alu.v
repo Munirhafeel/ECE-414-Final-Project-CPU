9768630a1bb01df2a417d9727da29611d64c0c08
Munirhafeel <83468433+Munirhafeel@users.noreply.github.com>
2022-11-27 21:01:37 -0500

    ALU done

--- alu.v
+++ alu.v
@@ -2,32 +2,73 @@ module alu(
     input [3:0] FS, //functional select
     input reg [15:0] SR, //Rs -> source
     input reg [15:0] TR, //Rt -> transfer
-    output reg [15:0] DR, //Rd -> destiantion
+    output reg [15:0] DR, //Rd -> destination
+    output reg V,
+    output reg C,
+    output reg N,
     output reg Z //Z flag
 );
 
-always @ (*) //combinational logic
+always @ (FS or SR or TR or DR)
 begin
-    case(FS)
-    4'h0: DR <= SR + TR; // 0000 -> ADD
-    4'h1: DR <= SR - TR; // 0001 -> SUB
-    4'h2: DR <= SR & TR; // 0010 -> AND
-    4'h3: DR <= SR | TR; // 0011 -> OR
-    4'h4: DR <= SR ^ TR; // 0100 -> XOR
-    4'h5: DR <= ~ SR; //0101 -> NOT
-    4'h6: DR <= SR <<< 1; //0110 -> SLA
-    4'h7: DR <= SR >>> 1; //0111 -> SRA
-    4'h8: DR <= SR; //1000 -> LI
-    4'h9: DR <= SR; // 1001 -> LW
-    4'hA: DR <= TR; //1010 -> SW
-    4'hB: DR <= SR; //1011 -> BIZ
-    4'hC: DR <= SR; //1100 -> BNZ
-    4'hD: DR <= SR; //1101 -> JAL
-    4'hE: DR <= DR; //1110 -> JMP
-    4'hF: DR <= SR; //1111 -> JR
-    default: DR <=DR; //EOE
+
+    case(FS[2:0])
+    4'h0: // 0000 -> ADD
+        begin
+            {C,DR} = SR + TR;
+            V = (!DR[15] & SR[15] & TR[15]) | (DR[15] & !SR[15] & !TR[15]);
+        end
+    4'h1: // 0001 -> SUB
+        begin
+            {C,DR} = SR + TR;
+            V = (!DR[15] & SR[15] & TR[15]) | (DR[15] & !SR[15] & !TR[15]);
+        end
+    4'h2: // 0010 -> AND
+        begin
+            DR <= SR & TR;
+            C <= 0;
+            V <= 0;
+        end
+    4'h3: // 0011 -> OR
+        begin
+            DR <= SR | TR;
+            C <= 0;
+            V <= 0;
+        end
+    4'h4: // 0100 -> XOR
+        begin
+            DR <= SR ^ TR;
+            C <= 0;
+            V <= 0;
+        end
+    4'h5: //0101 -> NOT
+        begin
+            DR <= ~SR;
+            C <= 0;
+            V <= 0;
+        end
+    4'h6: //0110 -> SLA
+        begin
+            DR <= SR <<< 1;
+            C <= 0;
+            V <= 0;
+        end
+    4'h0: //0111 -> SRA
+        begin
+            DR <= SR >>> 1;
+            C <= 0;
+            V <= 0;
+        end
+    default: 
+        begin
+            DR <= 0;
+            C <= 0;
+            V <= 0;
+        end
+
 
     assign Z <= (DR == 16'b0) ? 1 : 0;
+    assign N <= (DR[15] == 0) ? 1 : 0;
     
 end
 endmodule
