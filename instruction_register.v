5d1b738ccf91cee5cee1c4aadbe84452002e5b8c
Andres <andres.sebpal@gmail.com>
2022-11-28 07:56:28 -0500

    changes to instruction register

--- instruction_register.v
+++ instruction_register.v
@@ -1,5 +1,27 @@
 module instruction_register(
-
+    input [15:0] instruction, // instruction fetched from ROM
+    input IL, // instruction load
+    input clk, reset,
+    output reg [3:0] opcode, DR, SA, SB // Destination Register, Source Register A, Source Register B
 );
 
-//boiler plate for IR
+reg [15:0] instruction_reg;
+
+always @ (negedge clk) begin
+    if (reset) begin
+        instruction_reg <= 16'b0;
+    end else begin
+        if (IL) begin
+            instruction_reg <= instruction;
+        end else begin
+            instruction_reg <= instruction_reg;
+        end
+    end
+
+    opcode <= instruction_reg[15:12];
+    DR     <= instruction_reg[11:8];
+    SA     <= instruction_reg[7:4];
+    SB     <= instruction_reg[3:0];
+end
+
+endmodule
