db82543a6f221c432f82b1dff72cc1acc9cff3c0
Andres <andres.sebpal@gmail.com>
2022-11-30 19:37:06 -0500

    changes to A-Bus output

--- register_file.v
+++ register_file.v
@@ -1,11 +1,10 @@
 `timescale 1ps/1ps
 
 module register_file (
-    // do not need temp registers since it is two-cycle (?)
     // Instruction (DR, SA, SB - MSB = 0) or Control Word (DX, AX, BX - MSB = 1)
     input [3:0] DA, AA, BA, // Destination Address, A-Address, B-Address
-    input [3:0] FS, // Function Select
     input [15:0] D, // D-Bus (Mux-D) - Data-Write to Destination Address
+    input [3:0] FS, // Function Select
     input RW, // Register Write
     input clk, reset,
     output [15:0] A, B // A-Bus, B-Bus
@@ -13,13 +12,13 @@ module register_file (
 
 reg [15:0] register[15:0]; // 16x16 register file
 
-// Function Select - FS ?
-// A-Bus - AA -> different instructions ?
-assign A = register[AA]; // destination register for others
+// BIZ (1011) - RA -> RD
+// BNZ (1100) - RA -> RD
+assign A = (FS == 4'b1011 || FS == 4'b1100) ? register[DA] : register[AA]; // destination register for others
 assign B = register[BA];
 
-// reset or register write enable
 always @ (posedge clk) begin
+    // reset
     if (reset) begin
         register[0]  <= 16'b0;
         register[1]  <= 16'b0;
@@ -38,6 +37,7 @@ always @ (posedge clk) begin
         register[14] <= 16'b0;
         register[15] <= 16'b0;
     end else begin
+        // register write enable
         if (RW)
             register[DA] <= D;
     end
