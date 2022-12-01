2e2116f54b2ec2a4e447c0c7595a70282dee14e4
Andres <andres.sebpal@gmail.com>
2022-12-01 08:36:32 -0500

    changes to control unit

--- control_unit.v
+++ control_unit.v
@@ -2,59 +2,52 @@
 
 module control_unit (
     input clk, reset,
-    input [15:0] instruction, // instruction fetched from ROM
-    input [15:0] bus_A // bus_A (Address-A Data)
-    input Z, // Z Flag | Signal
-    output [3:0] DR, SA, SB, // Instruction Register -> Destination Register, Source Register A, Source Register B
-    // DX, AX, BX
+    input [15:0] instruction,   // instruction fetched from ROM
+    input [15:0] A_bus          // A Bus (Address-A Data)
+    input Z,                    // Z Flag | Signal
+    output [3:0] DR, SA, SB,    // Instruction Register -> Destination Register, Source Register A, Source Register B
     output [5:0] PC,
-    // output MB, MM, MD, MW, RW, MP, // Control Word
     // Control Word
-    output MB, MD, RW, MM, MW, // MUX-B, MUX-D, Register Write, MUX-M, Memory Write
-    output FS // Function Select - ALU
+    output MB, MD, RW, MM, MW,  // MUX-B, MUX-D, Register Write, MUX-M, Memory Write
+    output [3:0] FS             // Function Select - ALU
 );
 
-wire [3:0] opcode; // instruction from instruction register
+wire [3:0] opcode_bus;
 // control word from control unit
-wire [1:0] PS; // PC Select
-wire NS, IL; // Next State | Instruction Load
+wire [1:0] PS_bus;      // PC Select
+wire NS_bus, pstate_bus, IL_bus;    // Next State | Instruction Load
 
 program_counter PC (
-    .DR(), .SB().   // Source-B
-    .D(),           // D-Bus (MUX-D) 0 = Load Immediate || 1 = Memory
-    .PS(),          // PS Signal from Control Word (Control Logic)
-    .clk(), .reset(),
-    .address()
+    .SA(SA), .SB(SB),
+    .A_bus(A_bus[5:0]),
+    .PS(PS_bus),
+    .clk(clk), .reset(reset),
+    .PC(PC)
 );
 
 instruction_register IR (
-    .instruction(),                 // instruction fetched from ROM
-    .IL(),                          // instruction load
-    .clk(), .reset(),
-    .opcode(), .DR(), .SA(), .SB()  // Destination Register, Source Register A, Source Register B
+    .instruction(instruction),
+    .IL(IL_bus),
+    .clk(clk), .reset(reset),
+    .opcode(opcode_bus), .DR(DR), .SA(SA), .SB(SB)
 );
 
 control_logic CL (
-    .state(), 
-    .Z(), //zero from ALU
-    .opcode(),
-    .eoe(), //pseudo end of execution
-    .next_state(),
-    .FS(),
-    .PS(),
-    .IL(), 
-    .MB(), 
-    .MD(), 
-    .RW(), 
-    .MM(), 
-    .MW(), 
-    .WP(), // Write PC
+    .state(pstate_bus),
+    .Z(Z),
+    .opcode(opcode_bus),
+    .eoe(instruction[11:8]),
+    .next_state(NS_bus),
+    .FS(FS),
+    .PS(PS_bus),
+    .IL(IL_bus),
+    .MB(MB), .MD(MD), .RW(RW), .MM(MM), .MW(MW)
 );
 
 control_state CS (
-    .NS(),
-    .clk(), .reset(),
-    .next_state()
+    .NS(NS_bus),
+    .clk(clk), .reset(clk),
+    .next_state(pstate_bus)
 );
 
 endmodule
