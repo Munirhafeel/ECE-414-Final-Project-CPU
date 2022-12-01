f15639bc3f719bac4a38f512f49e4e91a8acfdd9
Andres <andres.sebpal@gmail.com>
2022-11-30 22:40:14 -0500

    updates to datapath

--- datapath.v
+++ datapath.v
@@ -1,69 +1,48 @@
-module datapath(
-    input clk,
-    input reset,
-    input [15:0] DataIN, //Data In from RAM
-    input [3:0] DR,SA,SB,
+module datapath (
+    input clk, reset,
+    input [15:0] data_in, // data in from RAM
+    input [5:0] PC, // Address to RAM
+    input [3:0] DR, SA, SB,
+    input MB, MD, RW, MM, MW,
     input [3:0] FS,
-    input [5:0] PC,
-    input MB, MM, MW, MD, WP, RW,
-    output [15:0] D_OUT,
-    output [15:0] Address_OUT,
+    output [15:0] data_out, // data to RAM
+    output [15:0] address_out, // RA - Address A
     output V, C, N, Z
 ); 
 
-wire [15:0] RF_B, RF_A, Bus_A, Bus_B, Bus_D, PC_A, F_out;
+wire [15:0] A_bus, B_bus, MB_bus, F_Bus, D_Bus, M_Bus;
 
-
-//MUX for Data in 
-//not too sure about this
-assign PC_A <= WP ? {10'b0, PC} : D; 
-
-
-register_file RF(
-    .DA(DR),
-    .AA(SA),
-    .BA(BA),
+register_file RF (
+    .DA(DR), .AA(SA), .BA(BA),
+    .D(M_Bus),
     .FS(FS),
-    .D(PC_A),
     .RW(RW),
-    .clk(clk),
-    .reset(reset),
-    .A(RF_A),
-    .B(RF_B)
+    .clk(clk), .reset(reset),
+    .A(A_bus), .B(B_bus)
 );
 
-
 //MUX B
-assign Bus_B <= MB ? {8'b0,SA,SB} : Bus_B; 
+assign MB_bus = MB ? {8'b0,SA,SB} : Bus_B;
 
-//Mux MM
-
-assign Address_OUT <= MM ? PC_A : Bus_A;
-
-
-/*
-input [3:0] FS, //functional select
-    input [15:0] A, //Rs -> source
-    input [15:0] B, //Rt -> transfer
-    output reg [15:0] F, //Rd -> destination
-    output reg V,
-    output reg C,
-    output reg N,
-    output reg Z //Z flag*/
-
-alu ALU(
+alu ALU (
     .FS(FS),
     .A(Bus_A),
-    .B(Bus_B), 
-    .F(F_out),
+    .B(MB_bus),
+    .data_out(F_Bus),
     .V(V),
     .C(C),
     .N(N)
     .Z(Z)
 );
 
-//mux D
+// MUX D
+assign D_Bus = MD ? data_in : F_Bus;
 
-assign Bus_D <= MD ? DataIN : F_out;
+//Mux MM
+assign M_Bus = MM ? ({10'b0,PC} + 1'b1) : D_Bus;
+
+assign data_out = MB_bus;
+
+assign address_out = Bus_A;
 
 endmodule; 
